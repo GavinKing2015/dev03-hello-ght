7818d5a21c1046d096d3c53f4c640dc44373c3fc
Whitehawk2015 <85392086+Whitehawk2015@users.noreply.github.com>
2021-06-21 20:33:26 -0500

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,12 @@
 //jQuery
 $(document).ready(function () {
-  console.log("Hello, Local!");
+  console.log("Hello,Github!");
   //JavaScript
   // Select the element and store it in a variable.
   var el = document.getElementById("my-greeting");
   // Change the value of the class attribute.
-  el.innerText = "Hello, Local! (programtically.)";
+  el.innerText = "Hello,Github! (programtically.)";
 
   //let's do it again with jQ
-  $("#my-greeting").text("hello Github pages");
+  $("#my-greeting").text("hello Github");
 });
